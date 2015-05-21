2a0b6cc11c3bf569565540327e4b800c3a1b6da0
Robert Kolozsi <robert.kolozsi@gmail.com>
2015-05-21 10:19:08 +0200

    Fixed urls in tempaltes to correspond to a new setup.

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'register-form/$', 'register.views.process_register', name='register-form'),
+    url(r'register-form/$', 'register.views.process_register', name='index'),
     url(r'thanks-for-registering/$', 'register.views.registered_successfully', name='thanks'),
 )
