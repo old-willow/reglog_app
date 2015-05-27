8bb08c67a7aff944cecc98ba95f76f78d79afb1d
Robert Kolozsi <robert.kolozsi@gmail.com>
2015-05-27 12:58:52 +0200

    Added django.contrib.sites app to project. email system requires it.

--- reglog_app/settings.py
+++ reglog_app/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'home',
     'login',
     'register',
 )
+
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
