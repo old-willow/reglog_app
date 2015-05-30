a82bbb0e297b485d5b991bd615e2198962dd153f
Robert Kolozsi <robert.kolozsi@gmail.com>
2015-05-31 00:11:41 +0200

    Changed the pattern to match the new django version for token and udb64 for password reset confirm.

--- login/urls.py
+++ login/urls.py
@@ -38,12 +38,17 @@ urlpatterns = patterns('',
        name='password_reset_done'),
 
     # view: password_reset_confirm
-    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'django.contrib.auth.views.password_reset_confirm',
         {'template_name': 'login/password_reset_confirm.html',
          'post_reset_redirect': 'login:password_reset_complete',
          'set_password_form': ChangePasswordForm },
          name='password_reset_confirm'),
+
+    # Original
+    #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #    'django.contrib.auth.views.password_reset_confirm',
+    #    name='password_reset_confirm'),
 
     # view: password_reset_complete
     #url(r'^password/reset/complete/$',
