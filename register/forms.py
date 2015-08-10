d704ab022ce71a202d5fdc93745a7e2618e730c3
old-willow <robert.kolozsi@gmail.com>
2015-08-10 16:52:32 +0200

    Cleaned very ugly file forms.py in register app.

--- register/forms.py
+++ register/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.admin import widgets # for the built in django datepicker.
+#from django.contrib.admin import widgets  # for the built in django datepicker.
 
 from register.models import User_Data, Country
 
-from datetime import datetime
+#from datetime import datetime
 
 
 class RegistrationFormCustom(forms.Form):
@@ -18,33 +18,31 @@ class RegistrationFormCustom(forms.Form):
 
     gender = forms.ChoiceField(required=False,
                                choices=GENDER_CHOICE,
-                               label='Gender:')
-    firstname = forms.CharField(label='First name:')
-    lastname = forms.CharField(label='Last name:')
-    username = forms.CharField(label='User name:')
-    phone = forms.CharField(label='Phone number:', required=False)
+                               label='Gender')
+    firstname = forms.CharField(label='First name')
+    lastname = forms.CharField(label='Last name')
+    username = forms.CharField(label='User name')
+    phone = forms.CharField(label='Phone number', required=False)
     countries = forms.ModelChoiceField(queryset=Country.objects.all(),
-                                       label='Country:',
+                                       label='Country',
                                        empty_label='-- --')
-    address = forms.CharField(widget=forms.Textarea, label='Address:')
-    email = forms.EmailField(label='e-mail:')
-    webpage = forms.URLField(required=False, label='Web page:')
+    address = forms.CharField(widget=forms.Textarea, label='Address')
+    email = forms.EmailField(label='e-mail')
+    webpage = forms.URLField(required=False, label='Web page')
     password = forms.CharField(widget=forms.PasswordInput(render_value=False),
                                max_length=15, min_length=4,
-                               label='Password:')
+                               label='Password')
     confirm_password = forms.CharField(widget=forms.PasswordInput(render_value=False),
                                        max_length=15, min_length=4,
-                                       label='Confirm password:')
-    datepicker = forms.DateField(required=False, label='Date of birth:')
-    note = forms.CharField(widget=forms.Textarea, required=False, label='Notes:')
+                                       label='Confirm password')
+    datepicker = forms.DateField(required=False, label='Date of birth')
+    note = forms.CharField(widget=forms.Textarea, required=False, label='Notes')
     portrait = forms.ImageField(required=False,
                                 #max_length=5,
-                                label='Portrait picture:')
-
+                                label='Portrait picture')
 
     def __unicode__(self):
         return u"%s %s" % (self.firstname, self.lastname)
-
 
     def clean_username(self):
         """
@@ -57,12 +55,11 @@ class RegistrationFormCustom(forms.Form):
             # Find username in database if exists.
             User.objects.get(username=self.cleaned_data['username'])
 
-        except User.DoesNotExist: # If no such username return data.
+        except User.DoesNotExist:  # If no such username return data.
             return self.cleaned_data['username']
 
         # Else, if there is already a username raise exception!
         raise forms.ValidationError('Username already exists. Please submit another one.')
-
 
     def clean_portrait(self):
         """
@@ -75,7 +72,6 @@ class RegistrationFormCustom(forms.Form):
                 return self.cleaned_data['portrait']
             else:
                 raise forms.ValidationError('Only allowed \'jpg\' files!')
-
 
     def clean_email(self):
         """
@@ -92,16 +88,14 @@ class RegistrationFormCustom(forms.Form):
         raise forms.ValidationError(
             'This email is already in use, please choose another one.')
 
-
     def save(self):
         """
         First storing data to the User database,
         then storing my additional data to my database.
         """
-        django_user = User.objects.create_user(
-            username = self.cleaned_data['username'],
-            email = self.cleaned_data['email'],
-            password = self.cleaned_data['password'])
+        django_user = User.objects.create_user(username=self.cleaned_data['username'],
+                                               email=self.cleaned_data['email'],
+                                               password=self.cleaned_data['password'])
 
         # Storing the rest of user data in 'auth_user' django table.
         django_user.first_name = self.cleaned_data['firstname']
@@ -109,19 +103,17 @@ class RegistrationFormCustom(forms.Form):
 
         django_user.save()
 
-        extend_user_data = User_Data.objects.create(
-            user_id = django_user.id,
-            gender = self.cleaned_data['gender'],
-            phone = self.cleaned_data['phone'],
-            countries = self.cleaned_data['countries'],
-            address = self.cleaned_data['address'],
-            webpage = self.cleaned_data['webpage'],
-            date_of_birth = self.cleaned_data['datepicker'],
-            note = self.cleaned_data['note'],
-            portrait = self.cleaned_data['portrait'])
+        extend_user_data = User_Data.objects.create(user_id=django_user.id,
+                                                    gender=self.cleaned_data['gender'],
+                                                    phone=self.cleaned_data['phone'],
+                                                    countries=self.cleaned_data['countries'],
+                                                    address=self.cleaned_data['address'],
+                                                    webpage=self.cleaned_data['webpage'],
+                                                    date_of_birth=self.cleaned_data['datepicker'],
+                                                    note=self.cleaned_data['note'],
+                                                    portrait=self.cleaned_data['portrait'])
 
         return extend_user_data
-
 
     def clean(self):
         if 'password' in self.cleaned_data and 'confirm_password' in self.cleaned_data:
