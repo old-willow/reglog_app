6915147955d5b5d5c0c3e8639d593dfb8214a0bc
old-willow <robert.kolozsi@gmail.com>
2015-08-12 16:01:18 +0200

    Added tests for user existance via ajax.

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,8 @@
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 from django.core import mail
 
 
@@ -29,6 +32,36 @@ class ReglogBasicTest(unittest.TestCase):
         self.browser.get('http://127.0.0.1:8000/login/password_reset/')
 
         self.assertEqual("Password Reset Form", self.browser.title)
+
+
+class LoginTests(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.close()
+
+    def test_check_login_ajax_with_existing_user(self):
+        """
+        Checks if existing user is given.
+        """
+        self.browser.get('http://127.0.0.1:8000/login/')
+        username_element = self.browser.find_element_by_name('username')
+        username_element.send_keys('robi', Keys.TAB)
+
+        self.assertNotIn('No such user!', self.browser.page_source)
+        #self.browser.switch_to_alert()
+
+    def test_check_login_ajax_with_non_existing_user(self):
+        """
+        Checks if non existing user is given.
+        """
+        self.browser.get('http://127.0.0.1:8000/login/')
+        username_element = self.browser.find_element_by_name('username')
+        username_element.send_keys('fobi', Keys.TAB)
+        self.browser.implicitly_wait(3)
+
+        self.assertIn('No such user!', self.browser.page_source)
 
 
 class SendEmailTest(unittest.TestCase):
