00c9b0adfea4f6b0f1a6e0673dfca2c7a49c7245
old-willow <robert.kolozsi@gmail.com>
2015-08-12 14:56:59 +0200

    Added a bunch of simple tests and email test.

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,9 @@
 import unittest
 from selenium import webdriver
+from django.core import mail
 
 
-class ReglogHomePageTest(unittest.TestCase):
+class ReglogBasicTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -12,7 +13,39 @@ class ReglogHomePageTest(unittest.TestCase):
     def test_get_home_page(self):
         self.browser.get('http://127.0.0.1:8000')
 
-        assert "Home Page for" in self.browser.title
+        self.assertIn("Home Page for", self.browser.title)
+
+    def test_get_login_page(self):
+        self.browser.get('http://127.0.0.1:8000/login/')
+
+        self.assertEqual("Login Form", self.browser.title)
+
+    def test_get_register_page(self):
+        self.browser.get('http://127.0.0.1:8000/register/register-form/')
+
+        self.assertEqual("Registration Form", self.browser.title)
+
+    def test_get_password_reset_page(self):
+        self.browser.get('http://127.0.0.1:8000/login/password_reset/')
+
+        self.assertEqual("Password Reset Form", self.browser.title)
+
+
+class SendEmailTest(unittest.TestCase):
+    def test_send_mail(self):
+        """
+        Basic email sending test.
+        """
+        from django.conf import settings
+
+        settings.configure(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+
+        mail.send_mail('Test subject', 'Message part of the email.',
+                       'robert.kolozsi@gmail.com', ['robert.kolozsi@gmail.com', ],
+                       fail_silently=False)
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'Test subject')
 
 
 if __name__ == '__main__':
